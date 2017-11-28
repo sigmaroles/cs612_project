d93c312c1351e762632bf40567dbe4b86872d949
sigmaroles <sohhom@gmail.com>
2017-11-29 00:27:32 +0530

    comments

--- gen_heatmaps.py
+++ gen_heatmaps.py
@@ -9,9 +9,9 @@ import seaborn as sns
 s_MAX = 2**14
 s_MIN = 2**11
 s_STEP = 2**11
-t_MAX = 1000
-t_MIN = 30
-t_STEP = 85
+t_MAX = 30
+t_MIN = 500
+t_STEP = 1200
 
 
 """
@@ -28,6 +28,7 @@ def isSpatiallyLocal(row, s):
         return (a1<=a2+s) and (a2+n2>=a1)
     
 if __name__=='__main__':
+    # yadda yadda .. matplotlib settings to make it look good
     font = {'family' : 'sans-serif',
         'size'   : 16,
         'weight' : 'bold',
@@ -35,37 +36,51 @@ if __name__=='__main__':
     axes = { 'labelsize': 'x-large' }
     matplotlib.rc('font', **font)
     matplotlib.rc('axes', **axes)
-    #plt.rcParams["figure.figsize"] = [26, 13]
-    
+   
+    # location of trace files w.r.t current dir
     data_dir = 'Trace_files'
+    # get the names of all .txt files under trace file directory
     fnames = [x for x in filter(lambda x: x.endswith('.txt'), os.listdir(os.path.join('.', data_dir)))]
+    # extract names of workloads, will be useful later
     workload_names = [ x for x in map(lambda x: x.split('_')[1].split('.')[0], fnames) ]
 
-    col_names = ['a', 'n']
+    # create Pandas dataframe from the trace files. 
+    # Only read columns 0 and 2
+    # dataset is a dict, indexed by strings in workload_names
+    # e.g. dataset['radio']
     dataset = {fname.split('_')[1].split('.')[0]:pd.read_csv(os.path.join(data_dir, fname), usecols=[0,2], delimiter='\s+', header = None, dtype =int) for fname in fnames}
         
+    # labels for the heatmap axes
     t_ax = [x for x in range(t_MIN,t_MAX,t_STEP)]
     s_ax = [x for x in range(s_MIN, s_MAX, s_STEP)] 
     
-    # main loop - for each workload in dataset...
+    # main loop - for each workload in dataset... 
     for wname in workload_names:
         fig, ax = plt.subplots(figsize=(28,11))
         print ("Beginning with ",wname)
-        #data = [tuple(x) for x in dataset[wname].values]
+        # df is just a handy shortcut
         df = dataset[wname].values
+        # init the empty list to hold the heatmap matrix
         kst = []
         for t in range(t_MIN,t_MAX,t_STEP):
-            # init the container for storing the row for this t
+            # init the empty list for storing a row
             kst_row = []
             # get pairs of memory references that are t steps apart in the file
             tlist = np.hstack([df[t:], df[:-t]])
+            # the number of references in this list of references (for this t)
             L = len(tlist)
+            # now, given the t list, for each s...
             for s in range(s_MIN, s_MAX, s_STEP):
-                # a list of booleans, indicating locality of each pair in tlist
+                # calculate the local/non-local ness of each item in tlist
                 local = [isSpatiallyLocal(x, s) for x in tlist]
-                kst_row.append(sum(local) / np.abs(L - t))
+                # count the matrix value (probability) and store it
+                kst_row.append(sum(local) / np.abs(L-t))
+            # display progress..it can get quite slow!
             print ("\tt = ", t)
+            # save the row to matrix
             kst.append(kst_row)
+        
+        # plot and save the matrix (heatmap)
         kst = np.array(kst)
         sns.heatmap(kst, ax=ax, robust=True, annot=True, linewidth=0, xticklabels=s_ax, yticklabels=t_ax, center=kst.max())
         ax.set_ylabel('temporal (instructions)', fontsize=18)
